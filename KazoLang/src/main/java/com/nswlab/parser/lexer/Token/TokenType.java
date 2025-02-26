03d747145099244bd90348f15a6c8a24a2fc359b
Mutarov <129605231+Mutarov@users.noreply.github.com>
2025-02-26 09:14:48 +0300

    Added more comments and 1 small fix

--- KazoLang/src/main/java/com/nswlab/parser/lexer/Token/TokenType.java
+++ KazoLang/src/main/java/com/nswlab/parser/lexer/Token/TokenType.java
@@ -1,28 +1,28 @@
 package com.nswlab.parser.lexer.Token;
 
 public enum TokenType {
-    KEYWORD,    // if, else
-    IDENTIFIER, // var1
-    PLUS,  // +
-    MINUS, //
-    MUL, //  
-    DIV,
-    EQUAL,
-    NOT_EQUAL,
-    LESS,
-    LESS_EQUAL,
-    GREATER,
-    GREATER_EQUAL,
-    ASSIGN,
-    DOT,
-    PRINT,      // пока до стд либ
-    NLITERAL,   // 5
-    STRLITERAL, // "строки"
-    FLTLITERAL, // 3.14
-    SEMICOLON,  // ;
-    LPAREN,     // (
-    RPAREN,     // )
-    LBRACE,     // {
-    RBRACE,     // }
-    EOF,        // EOF
+    KEYWORD,       // if, else
+    IDENTIFIER,    // var1
+    PLUS,          // +
+    MINUS,         // -
+    MUL,           // *
+    DIV,           // /
+    EQUAL,         // ==
+    NOT_EQUAL,     // !=
+    LESS,          // <
+    LESS_EQUAL,    // <=
+    GREATER,       // >
+    GREATER_EQUAL, // >=
+    ASSIGN,        // =
+    DOT,           // .
+    PRINT,         // пока до стд либ
+    NLITERAL,      // 5
+    STRLITERAL,    // "строки"
+    FLTLITERAL,    // 3.14
+    SEMICOLON,     // ;
+    LPAREN,        // (
+    RPAREN,        // )
+    LBRACE,        // {
+    RBRACE,        // }
+    EOF,           // EOF
 }
